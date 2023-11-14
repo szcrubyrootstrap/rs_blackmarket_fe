5f00df025c056f0da71f9f15ead22cd9903c751b
sebastian zapata <sebastian.zapata@rootstrap.com>
2023-11-14 12:50:38 -0500

    [session_manager] Solving CR tips VII

--- src/templates/SessionTemplate/error.jsx
+++ src/templates/SessionTemplate/error.jsx
@@ -1,12 +1,16 @@
 import useUser from 'hooks/useUser'
 import { isRegistration, isLogin } from 'src/setup'
 
+const addSpace = (formName) => {
+  return isRegistration(formName) ? 'separation' : ''
+}
+
 export default function Error ({formName}) {
   const {loginError, loginErrorMessage} = useUser()
   const {registrationError, registrationErrorMessage} = useUser()
 
   return (
-    <div className="error-messages">
+    <div className={`error-messages ${addSpace(formName)}`}>
       { isLogin(formName) && loginError && <strong>{loginErrorMessage}</strong> }
       { isRegistration(formName) && registrationError && <strong>{registrationErrorMessage}</strong> }
     </div>
--- src/templates/SessionTemplate/footer.jsx
+++ src/templates/SessionTemplate/footer.jsx
@@ -3,9 +3,13 @@ import ForgotPassword from 'templates/SessionTemplate/forgotPassword'
 import PoliciesAcounts from 'templates/SessionTemplate/policiesAccount'
 import { isRegistration, isLogin } from 'src/setup'
 
+const addPadding = (formName) => {
+  return isRegistration(formName) ? 'no-padding' : ''
+}
+
 export default function Footer ({formName}) {
   return (
-    <div className="footer flex-column">
+    <div className={`footer flex-column ${addPadding(formName)}`}>
       <Error formName={formName} />
       { isLogin(formName) && <ForgotPassword /> }
       { isRegistration(formName) && <PoliciesAcounts /> }
